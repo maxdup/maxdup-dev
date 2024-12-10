c2d7dd1836c9a02140280e17be453ffaf9e20bd5
maxdup <mdupuis@hotmail.ca>
2024-12-09 21:30:59 -0500

    added dark theme favicon

--- webpack.common.js
+++ webpack.common.js
@@ -1,83 +1,87 @@
-const webpack = require('webpack');
+const webpack = require("webpack");
 
-const path = require('path');
-const dirSrc = path.join(__dirname, 'src');
-const dirBuild = path.join(__dirname, 'build');
+const path = require("path");
+const dirSrc = path.join(__dirname, "src");
+const dirBuild = path.join(__dirname, "build");
 
-const CopyWebpackPlugin = require('copy-webpack-plugin');
-const MiniCssExtractPlugin = require('mini-css-extract-plugin');
-const FaviconsWebpackPlugin = require('favicons-webpack-plugin');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const ExtractLocStrings = require('./localization/extract-strings.js');
+const CopyWebpackPlugin = require("copy-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const FaviconsWebpackPlugin = require("favicons-webpack-plugin");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const ExtractLocStrings = require("./localization/extract-strings.js");
 
 const commonConfig = {
-  context: path.join(__dirname, 'src'),
+  context: path.join(__dirname, "src"),
   entry: {
-    index: './index.js',
+    index: "./index.js",
   },
   output: {
     path: dirBuild,
-    filename: '[name].[contenthash].bundle.js',
-    chunkFilename: '[name].[contenthash].bundle.js'
+    filename: "[name].[contenthash].bundle.js",
+    chunkFilename: "[name].[contenthash].bundle.js",
   },
   devServer: {
     port: 8000,
-    watchFiles: ['src/**/*']
+    watchFiles: ["src/**/*"],
   },
   module: {
-    rules: [{
-      test: /\.(glsl|vs|fs|vert|frag)$/,
-      loader: 'ts-shader-loader'
-    }, {
+    rules: [
+      {
+        test: /\.(glsl|vs|fs|vert|frag)$/,
+        loader: "ts-shader-loader",
+      },
+      {
         test: /\.m?js$/,
         exclude: /node_modules/,
         use: {
           loader: "babel-loader",
           options: {
-            presets: ['@babel/preset-env']
-          }
-        }
-    }, {
-      test: /\.font\.js/,
-      use: [
-        MiniCssExtractPlugin.loader,
-        {
-          loader: 'css-loader',
-          options: {
-            url: false
-          }
+            presets: ["@babel/preset-env"],
+          },
         },
-        'webfonts-loader'
-      ]
-    }, {
-      test: /\.(woff|woff2|eot|ttf|otf|svg|jpg|jpeg|png|gif|webp)$/i,
-      type: "asset/resource",
-    }]
+      },
+      {
+        test: /\.font\.js/,
+        use: [
+          MiniCssExtractPlugin.loader,
+          {
+            loader: "css-loader",
+            options: {
+              url: false,
+            },
+          },
+          "webfonts-loader",
+        ],
+      },
+      {
+        test: /\.(woff|woff2|eot|ttf|otf|svg|jpg|jpeg|png|gif|webp)$/i,
+        type: "asset/resource",
+      },
+    ],
   },
   plugins: [
-    new ExtractLocStrings({locales:['en-US', 'fr-CA', 'lo-IP']}),
+    new ExtractLocStrings({ locales: ["en-US", "fr-CA", "lo-IP"] }),
     new MiniCssExtractPlugin({
-      filename: '[name].[contenthash].css'
+      filename: "[name].[contenthash].css",
     }),
     new HtmlWebpackPlugin({
-      template: path.join(dirSrc, 'index.ejs'),
-      filename: 'index.html',
-      chunks: ['index'],
-      baseUrl: 'https://maxdup.dev',
+      template: path.join(dirSrc, "index.ejs"),
+      filename: "index.html",
+      chunks: ["index"],
+      baseUrl: "https://maxdup.dev",
     }),
     new FaviconsWebpackPlugin({
-      logo: path.join(dirSrc, './images/favicon.png'),
-      mode: 'webapp',
+      logo: path.join(dirSrc, "./images/favicon.svg"),
+      mode: "webapp",
       favicons: {
-        background: '#000',
-        theme_color: '#333',
-      }
+        background: "#000",
+        theme_color: "#333",
+      },
     }),
     new CopyWebpackPlugin({
-      patterns: [{from: 'static', to: 'static'}]
+      patterns: [{ from: "static", to: "static" }],
     }),
-
-  ]
-}
+  ],
+};
 
 module.exports = commonConfig;
