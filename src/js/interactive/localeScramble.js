8bea7a35a29e16c7eee859a90c4ec19797e618dc
maxdup <mdupuis@hotmail.ca>
2025-01-16 00:02:58 -0500

    fixed various spacing issues

--- src/js/interactive/localeScramble.js
+++ src/js/interactive/localeScramble.js
@@ -1,5 +1,5 @@
-import mainLoop from "../main-loop.js";
-import store from "../store";
+import mainLoop from '../main-loop.js';
+import store from '../store';
 
 const MIN_DURATION = 200;
 const MAX_DURATION = 1250;
@@ -14,14 +14,17 @@ function LocaleScramble(){
     this.baseLocale = LOCALIZED_TARGETS[this.targetLocaleId];
     this.targetLocale = LOCALIZED_TARGETS[this.targetLocaleId];
     this.applyLocale(true);
-  }
+  };
 
-  this.localizedElementsText =
-    window.document.querySelectorAll("[data-localize-text]");
-  this.localizedElementsAria =
-    window.document.querySelectorAll("[data-localize-aria]");
-  this.localizedElementsContent =
-    window.document.querySelectorAll("[data-localize-content]");
+  this.localizedElementsText = window.document.querySelectorAll(
+    '[data-localize-text]',
+  );
+  this.localizedElementsAria = window.document.querySelectorAll(
+    '[data-localize-aria]',
+  );
+  this.localizedElementsContent = window.document.querySelectorAll(
+    '[data-localize-content]',
+  );
 
   let insertLocaleSwitcher = () => {
     const container = document.createElement('ul');
@@ -43,61 +46,66 @@ function LocaleScramble(){
     const enLi = document.createElement('li');
     enLi.appendChild(enBtn);
     container.appendChild(enLi);
-  }
+  };
 
   this.setTargetLocale = (locale) => {
     this.baseLocale = this.targetLocale;
     this.targetLocale = locale;
     this.applyLocale();
-  }
+  };
 
   this.toggleLocale = () => {
     this.baseLocale = LOCALIZED_TARGETS[this.targetLocaleId];
     this.targetLocaleId = (this.targetLocaleId + 1) % LOCALIZED_TARGETS.length;
     this.targetLocale = LOCALIZED_TARGETS[this.targetLocaleId];
     this.applyLocale();
-  }
+  };
 
   const getLocString = (hash, locale) => {
-    return LOCALIZED_STRINGS[`${hash}:${locale}`] || "";
-  }
+    return LOCALIZED_STRINGS[`${hash}:${locale}`] || '';
+  };
 
   const baseStringFN = (e, _i) => {
-    const hash = e.getAttribute("data-localize-text");
+    const hash = e.getAttribute('data-localize-text');
     return getLocString(hash, this.baseLocale);
-  }
+  };
 
   const targetStringFN = (e, _i) => {
-    const hash = e.getAttribute("data-localize-text");
+    const hash = e.getAttribute('data-localize-text');
     return getLocString(hash, this.targetLocale);
-  }
+  };
 
   this.setElemText = (e) => {
-    const hash = e.getAttribute("data-localize-text");
+    const hash = e.getAttribute('data-localize-text');
     e.innerHTML = getLocString(hash, this.targetLocale);
-  }
+  };
 
   this.setElemAria = (e) => {
-    const hash = e.getAttribute("data-localize-aria");
-    e.setAttribute("aria-label", getLocString(hash, this.targetLocale));
-  }
+    const hash = e.getAttribute('data-localize-aria');
+    e.setAttribute('aria-label', getLocString(hash, this.targetLocale));
+  };
 
   this.setElemContent = (e) => {
-    const hash = e.getAttribute("data-localize-content");
-    e.setAttribute("content", getLocString(hash, this.targetLocale));
-  }
+    const hash = e.getAttribute('data-localize-content');
+    e.setAttribute('content', getLocString(hash, this.targetLocale));
+  };
 
   this.applyLocale = (instant) => {
-    store.savePreference("locale", this.targetLocale);
+    store.savePreference('locale', this.targetLocale);
     mainLoop.singleScramble.clearSequences();
     if (instant) {
       this.localizedElementsText.forEach(this.setElemText);
     } else {
       mainLoop.singleScramble.sequence(
-        [...this.localizedElementsText], MIN_DURATION, MAX_DURATION,
-        baseStringFN, targetStringFN, true);
+        [...this.localizedElementsText],
+        MIN_DURATION,
+        MAX_DURATION,
+        baseStringFN,
+        targetStringFN,
+        true,
+      );
     }
-  }
+  };
 
   init();
 }
