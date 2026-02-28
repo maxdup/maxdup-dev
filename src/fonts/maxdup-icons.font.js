2a43530004874c1b46d0a5680be2a32aaf42595c
maxdup <mdupuis@hotmail.ca>
2026-02-28 15:06:28 -0500

    feat: updated packages and scss syntax

--- src/fonts/maxdup-icons.font.js
+++ src/fonts/maxdup-icons.font.js
@@ -1,8 +1,8 @@
 module.exports = {
-  'files': [
+  files: [
     './icons/*.svg',
-    '../../node_modules/simple-icons/icons/github.svg*',
-    '../../node_modules/simple-icons/icons/linkedin.svg*',
+    '../../node_modules/bootstrap-icons/icons/github.svg*',
+    '../../node_modules/bootstrap-icons/icons/linkedin.svg*',
     '../../node_modules/boxicons/svg/regular/bx-menu.svg*',
     '../../node_modules/boxicons/svg/solid/bxs-phone.svg*',
     '../../node_modules/boxicons/svg/solid/bxs-envelope.svg*',
@@ -12,18 +12,18 @@ module.exports = {
     '../../node_modules/boxicons/svg/regular/bx-sun.svg*',
     '../../node_modules/boxicons/svg/regular/bx-moon.svg*',
   ],
-  'fontName': 'maxdup_icons',
-  'cssTemplate': './maxdup-icons.css.hbs',
-  'baseSelector': '.mdi',
-  'types': ['eot', 'woff', 'woff2', 'ttf', 'svg'],
-  'fixedWidth': false,
-  'fileName': 'app.[fontname].[chunkhash].[ext]',
+  fontName: 'maxdup_icons',
+  cssTemplate: './maxdup-icons.css.hbs',
+  baseSelector: '.mdi',
+  types: ['eot', 'woff', 'woff2', 'ttf', 'svg'],
+  fixedWidth: false,
+  fileName: 'app.[fontname].[chunkhash].[ext]',
 
   // html doc
-  'html': true,
-  'htmlTemplate': './maxdup-icons.html.hbs',
+  html: true,
+  htmlTemplate: './maxdup-icons.html.hbs',
 
   // file output
-  'dest': 'static',
-  'writeFiles': true
+  dest: 'static',
+  writeFiles: true,
 };
